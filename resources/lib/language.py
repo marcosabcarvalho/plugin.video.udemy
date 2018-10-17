92c9e37ef6fa7d7d1aee8a0314786e75276a0392
Matt Huisman <me@matthuisman.nz>
2018-10-17 15:24:44 +1300

    Migrate to latest common

--- resources/lib/language.py
+++ resources/lib/language.py
@@ -1,17 +1,21 @@
 from matthuisman.language import BaseLanguage
 
 class Language(BaseLanguage):
-    LOGIN            = 30000
     MY_COURSES       = 30001
-    LOGOUT           = 30002
-    SETTINGS         = 30003
     ASK_USERNAME     = 30004
     ASK_PASSWORD     = 30005
     LOGIN_ERROR      = 30006
-    LOGOUT_YES_NO    = 30007
     COURSE_INFO      = 30008
     SECTION_LABEL    = 30009
     NO_STREAM_ERROR  = 30010
     NO_COURSES       = 30011
+    MAX_QUALITY      = 30012
+    Q_144P           = 30013
+    Q_360P           = 30014
+    Q_480P           = 30015
+    Q_720P           = 30016
+    Q_1080P          = 30017
+    BUSINESS_ACCOUNT = 30018
+    BUSINESS_NAME    = 30019
 
 _ = Language()
